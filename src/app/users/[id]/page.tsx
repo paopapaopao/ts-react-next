83ca2dc292e756942e5712356f2f4e4b3215be8f
paopapaopao <paolo.casugay@gmail.com>
2025-09-19 11:22:33 +0800

    Refactor

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { type Metadata } from 'next';
-import { type JSX, use } from 'react';
+import { type JSX } from 'react';
 
 import { PostList } from '@/components';
 
